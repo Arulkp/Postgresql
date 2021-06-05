ed6836881208f5466bd2f20a67b81c0d12e921e6
Arulkp <31948738+Arulkp@users.noreply.github.com>
2021-06-05 19:19:41 +0530

    Update rank.sql

--- rank.sql
+++ rank.sql
@@ -27,3 +27,7 @@ select name,salary,rank() over( partition by salary order by name desc)rank from
 select name,salary,dense_rank() over(partition by salary  order by name desc)rank from employee;
 select name,salary,row_number() over( partition by salary order by salary desc)rank from employee;
 select name,salary,ntile(3) over( order by salary desc)rank from employee;
+
+-- Get without active supplier product 
+with data as (select product_tmpl_id,string_agg(active_supplier::text,',') as asup from product_supplierinfo GROUP BY product_tmpl_id)
+select * from data WHERE asup not LIKE '%true%'
